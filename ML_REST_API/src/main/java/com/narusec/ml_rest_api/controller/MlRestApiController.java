1fd96e4007742fafbb1e3c9da205a3c209a10797
hyeondata <kimyonghyeondata@gmail.com>
2022-10-28 16:48:38 +0900

    [wip] python실행을 위한 매개변수 추가 및 Cotroller에서  test로 자기 자신에게 url날리는 python실행 코드 추가

--- ML_REST_API/src/main/java/com/narusec/ml_rest_api/controller/MlRestApiController.java
+++ ML_REST_API/src/main/java/com/narusec/ml_rest_api/controller/MlRestApiController.java
@@ -1,13 +1,17 @@
 package com.narusec.ml_rest_api.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.narusec.ml_rest_api.service.MlRestApiService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -17,15 +21,19 @@ public class MlRestApiController {
     @Autowired
     MlRestApiService mlRestApiService;
 
-    @RequestMapping(value = "Start", method = RequestMethod.GET)
-    public void executor(@RequestParam(value = "model")String model)throws InterruptedException{
+    @RequestMapping(value = "Start", method = RequestMethod.POST)
+    public void executor(@RequestBody Map<String, Object> map)throws InterruptedException{
         log.debug("start");
-/*        String [] parameter = new String[6];
+        String [] parameter = new String[6];
+        parameter[0] = map.get("m").toString();
+        parameter[1] = map.get("s").toString();
+        parameter[2] = map.get("e").toString();
+        parameter[3] = map.get("p").toString();
+        parameter[4] = map.get("c").toString();
+        parameter[5] = map.get("r").toString();
         mlRestApiService.pyStart(parameter);
-        System.out.println(parameter);*/
-
-        mlRestApiService.pyStart(model);
-        System.out.println(model);
+        System.out.println(parameter);
+        System.out.println(parameter[0]);
     }
 
     @RequestMapping(value = "End", method = RequestMethod.GET)
@@ -33,5 +41,45 @@ public class MlRestApiController {
         log.debug("start");
         mlRestApiService.pyStop(test);
         System.out.println(test);
+    }
+
+    @RequestMapping(value = "TEST", method = RequestMethod.GET)
+    public void executor(@RequestParam(value = "date")String date)throws Exception{
+        log.debug("Date");
+        Map<String,Object> requestParam = new HashMap<>();
+        requestParam.put("m","ST-MODEL1");
+        requestParam.put("s",date); //2022-10-22
+        requestParam.put("e","2022-10-24");
+        requestParam.put("p","1");
+        requestParam.put("c","{'l7':true}");
+        requestParam.put("r","{'bandwith':2}");
+        String json = new ObjectMapper().writeValueAsString(requestParam);
+        sendREST("http://localhost:8088/Start/",json);
+    }
+
+    public static void sendREST(String sendUrl, String jsonValue) throws IllegalStateException {
+        try{
+            log.debug("REST API Start");
+            URL url = new URL(sendUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept-Charset", "UTF-8");
+            conn.setConnectTimeout(10000);
+            conn.setReadTimeout(10000);
+            OutputStream os = conn.getOutputStream();
+            os.write(jsonValue.getBytes("UTF-8"));
+            os.flush();
+            if(conn.getResponseCode() == 200){
+                log.debug("REST API SUCCESS");
+            }else{
+                log.debug("REST API FAIL");
+            }
+            conn.disconnect();
+            log.debug("REST API End");
+        }catch(Exception e){
+            log.error(e.getMessage(), e); e.printStackTrace();
+        }
     }
 }
--- ML_REST_API/src/main/java/com/narusec/ml_rest_api/service/MlRestApiService.java
+++ ML_REST_API/src/main/java/com/narusec/ml_rest_api/service/MlRestApiService.java
@@ -26,41 +26,32 @@ public class MlRestApiService {
 
     }
 
-    public void pyStart(String parStrings){
+    public void pyStart(String parStrings[]){
         try{
             log.debug("Start");
-            String[] command = new String[3];
-            command[0] = "ssh conse@192.168.103.70 python3 /app/ad/train.py -m 'ST-MODEL1' -s '2022-10-22' -e '2022-10-24' -p 1 -c '{\"l7\":true}' -r '{\"bandwith\":2}'";
+//            String[] command = new String[3];
+//            command[0] = "ssh conse@192.168.103.70 python3 /app/ad/train.py -m 'ST-MODEL1' -s '2022-10-22' -e '2022-10-24' -p 1 -c '{\"l7\":true}' -r '{\"bandwith\":2}'";
             /*JSONParser jsonParser = new JSONParser();
             Object obj = jsonParser.parse()*/
-            /*
             String[] command = new String[8];
-            command[0] = "ssh conse@192.168.103.70 python3 ";
+            command[0] = "ssh conse@192.168.103.70 python3 /app/ad/train.py";
             command[1] = "-m " + parStrings[0];
             command[2] = "-s " + parStrings[1];
             command[3] = "-e " + parStrings[2];
             command[4] = "-p " + parStrings[3];
             command[5] = "-c " + parStrings[4];
             command[6] = "-r " + parStrings[5];
-*/
-
-            /*            String[] command = new String[8];
-            command[0] = pythonUrl;
-            command[1] = "-m " + parStrings[0];
-            command[2] = "-s " + parStrings[1];
-            command[3] = "-e " + parStrings[2];
-            command[4] = "-p " + parStrings[3];
-            command[5] = "-c " + parStrings[4];
-            command[6] = "-r " + parStrings[5];*/
             Thread thread = new Thread(new Runnable()  {
                 @Override
                 public void run()  {
                     excPython(command);
                  }
             });
-            thread.setName(parStrings);
+            thread.setName(parStrings[1]);
             thread.start();
+
             threads.add(thread);
+
         }catch (Exception e){
             System.out.println(e.getMessage());
             log.error(e.getMessage());
@@ -79,9 +70,9 @@ public class MlRestApiService {
     private void excPython(String[] command) {
         try {
             CommandLine commandLine = CommandLine.parse(command[0]);
-/*            for (int i = 1, n = command.length; i < n; i++) {
+            for (int i = 1, n = command.length; i < n; i++) {
                 commandLine.addArgument(command[i]);
-            }*/
+            }
 
             DefaultExecutor executor = new DefaultExecutor();
 
