e853ece1a82a4662acd1fd15f20c7d5bda01bc5f
hyeondata <kimyonghyeondata@gmail.com>
2023-01-04 17:02:06 +0900

    [add] ThresholdChange 파이썬 실행하는 API 코드 추가

--- ML_REST_API/src/main/java/com/narusec/ml_rest_api/controller/MlRestApiController.java
+++ ML_REST_API/src/main/java/com/narusec/ml_rest_api/controller/MlRestApiController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -22,7 +23,7 @@ public class MlRestApiController {
     MlRestApiService mlRestApiService;
 
     @RequestMapping(value = "MLRESTAPISTART", method = RequestMethod.POST)
-    public void executor(@RequestBody Map<String, Object> map)throws Exception{
+    public void executor(@RequestBody Map<String, Object> map) throws Exception {
         System.out.println(map);
         log.debug("start");
 /*
@@ -35,14 +36,14 @@ public class MlRestApiController {
         parameter[4] = map.get("p").toString();
 */
 
-        Map<String,Object> jsonParam = new HashMap<>();
-        jsonParam.put("-q ",map.get("seq").toString());
-        jsonParam.put("-m",map.get("m").toString());
-        jsonParam.put("-s",map.get("s").toString());
-        jsonParam.put("-e",map.get("e").toString());
-        jsonParam.put("-p ",map.get("p").toString());
-        jsonParam.put("-c ",new ObjectMapper().writeValueAsString(map.get("c")));
-        jsonParam.put("-r ",new ObjectMapper().writeValueAsString(map.get("r")));
+        Map<String, Object> jsonParam = new HashMap<>();
+        jsonParam.put("-q ", map.get("seq").toString());
+        jsonParam.put("-m", map.get("m").toString());
+        jsonParam.put("-s", map.get("s").toString());
+        jsonParam.put("-e", map.get("e").toString());
+        jsonParam.put("-p ", map.get("p").toString());
+        jsonParam.put("-c ", new ObjectMapper().writeValueAsString(map.get("c")));
+        jsonParam.put("-r ", new ObjectMapper().writeValueAsString(map.get("r")));
 /*
         parameter[5] = map.get("c").toString();
         parameter[6] = map.get("r").toString();
@@ -53,59 +54,90 @@ public class MlRestApiController {
     }
 
     @RequestMapping(value = "MLRESTAPISTOP", method = RequestMethod.POST)
-    public void stop(@RequestBody Map<String, Object> map)throws InterruptedException{
+    public void stop(@RequestBody Map<String, Object> map) throws InterruptedException {
         log.debug("start");
         mlRestApiService.pyStop(map.get("seq").toString());
         System.out.println(map.get("seq"));
     }
 
-    @RequestMapping(value = "MLRESPONSE",method = RequestMethod.GET)
+    @RequestMapping(value = "MLRESPONSE", method = RequestMethod.GET)
     @ResponseBody
-    public String responseApi(@RequestParam(value = "seq")String seq) throws Exception{
+    public String responseApi(@RequestParam(value = "seq") String seq) throws Exception {
         log.debug("responseApiStart");
 
-        return mlRestApiService.pythonResponse(seq);
+        return mlRestApiService.pythonResponse(seq, "");
+    }
+
+    @RequestMapping(value = "TholdChange", method = RequestMethod.POST)
+    @ResponseBody
+    public String threadhold(@RequestBody Map<String, Object> map) throws Exception {
+        log.debug("start");
+//        System.out.println(map);
+        return mlRestApiService.pythonResponse(map.get("seq").toString(), new ObjectMapper().writeValueAsString(map.get("data")));
+
+//        mlRestApiService.pythonResponse(map.get("seq").toString(),new ObjectMapper().writeValueAsString(map.get("data")));
+
     }
 
     @RequestMapping(value = "STOPTEST", method = RequestMethod.GET)  //데이터 받는 부분
-    public void stopTest(@RequestParam(value = "seq")String seq)throws Exception {
-        Map<String,Object> requestParam = new HashMap<>();
+    public void stopTest(@RequestParam(value = "seq") String seq) throws Exception {
+        Map<String, Object> requestParam = new HashMap<>();
 
-        requestParam.put("seq",seq);  //모델이름
+        requestParam.put("seq", seq);  //모델이름
 
         String json = new ObjectMapper().writeValueAsString(requestParam);
         System.out.println(json);
-        sendREST("http://localhost:8088/MLRESTAPISTOP/",json);
+        sendREST("http://localhost:8088/MLRESTAPISTOP/", json);
     }
+
     @RequestMapping(value = "TEST", method = RequestMethod.GET)  //데이터 받는 부분
-    public void executor()throws Exception{
-        Map<String,Object> requestParam = new HashMap<>();
-        Map<String,Object> map1 = new HashMap<>();
-        Map<String,Object> map2 = new HashMap<>();
+    public void executor() throws Exception {
+        Map<String, Object> requestParam = new HashMap<>();
+        Map<String, Object> map1 = new HashMap<>();
+        Map<String, Object> map2 = new HashMap<>();
 
         String a = "l7";
         String b = "bandwith";
 
-        map1.put(a,true);
-        map2.put(b,2);
+        map1.put(a, true);
+        map2.put(b, 2);
 
-        requestParam.put("m","ST-MODEL1");  //모델이름
-        requestParam.put("s","2022-10-22"); //2022-10-22  //시작 날짜
-        requestParam.put("e","2022-10-27"); //끝나는 날짜
-        requestParam.put("p","");  //주기
-        requestParam.put("seq","1231231");
-        requestParam.put("c",""); //전처리 관련 파라미터
-        requestParam.put("r",""); //모델 관련 파라미터
+        requestParam.put("m", "ST-MODEL1");  //모델이름
+        requestParam.put("s", "2022-10-22"); //2022-10-22  //시작 날짜
+        requestParam.put("e", "2022-10-27"); //끝나는 날짜
+        requestParam.put("p", "");  //주기
+        requestParam.put("seq", "1231231");
+        //   requestParam.put("c",""); //전처리 관련 파라미터
+        //   requestParam.put("r",""); //모델 관련 파라미터
+        requestParam.put("c", map1); //전처리 관련 파라미터
+        requestParam.put("r", map2); //모델 관련 파라미터
+        //이름 추가 필요
+        String json = new ObjectMapper().writeValueAsString(requestParam);
+        System.out.println(json);
+        sendREST("http://localhost:8088/MLRESTAPISTART/", json);
+    }
+
+    @RequestMapping(value = "TESTholdChange", method = RequestMethod.GET)  //데이터 받는 부분
+    public void executorTest() throws Exception {
+        Map<String, Object> requestParam = new HashMap<>();
+        Map<String, Object> map1 = new HashMap<>();
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("192.168.102.101", 3);
+//        map1.put("threshold","3");
+        map1.put("threshold", map2);
+        requestParam.put("seq", "221127");  //모델이름
+        requestParam.put("data", "{\'threshold\': {\'192.168.102.101\': -3.0671114350578588}}"); //모델 관련 파라미터
 //        requestParam.put("c",map1); //전처리 관련 파라미터
 //        requestParam.put("r",map2); //모델 관련 파라미터
         //이름 추가 필요
         String json = new ObjectMapper().writeValueAsString(requestParam);
-        System.out.println(json);
-        sendREST("http://localhost:8088/MLRESTAPISTART/",json);
+        System.out.println("testStart");
+        sendREST("http://localhost:8088/TholdChange/", json);
     }
 
     public static void sendREST(String sendUrl, String jsonValue) throws IllegalStateException {  //json 형식으로 변환 하여 POST로 데이터 날리는 부분
-        try{
+        try {
+            System.out.println("rrrrrrarrrstart");
             log.debug("REST API Start");
             URL url = new URL(sendUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -118,15 +150,16 @@ public class MlRestApiController {
             OutputStream os = conn.getOutputStream();
             os.write(jsonValue.getBytes("UTF-8"));
             os.flush();
-            if(conn.getResponseCode() == 200){
+            if (conn.getResponseCode() == 200) {
                 log.debug("REST API SUCCESS");
-            }else{
+            } else {
                 log.debug("REST API FAIL");
             }
             conn.disconnect();
             log.debug("REST API End");
-        }catch(Exception e){
-            log.error(e.getMessage(), e); e.printStackTrace();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            e.printStackTrace();
         }
     }
 }
--- ML_REST_API/src/main/java/com/narusec/ml_rest_api/service/MlRestApiService.java
+++ ML_REST_API/src/main/java/com/narusec/ml_rest_api/service/MlRestApiService.java
@@ -11,6 +11,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -28,7 +29,7 @@ public class MlRestApiService {
 
     private ArrayList<Thread> threads = new ArrayList<>();
 
-    private String[] pythonPM = new String[]{"-q ","-m","-s","-e","-p ","-c ","-r "};
+    private String[] pythonPM = new String[]{"-q ", "-m", "-s", "-e", "-p ", "-c ", "-r "};
 
     private List<String> command = new LinkedList<>();
 
@@ -36,24 +37,24 @@ public class MlRestApiService {
     private ModelInfoRepository modelInfoRepository;
 
 
-    public void pyStart(Map<String,Object> parStrings){
-        try{
+    public void pyStart(Map<String, Object> parStrings) {
+        try {
             log.debug("Start");
             command = new LinkedList<>();
 //            command.add("ssh conse@192.168.103.70 python3 /app/ad/train.py");  // 테스트시 사용
             command.add("python3 /app/ad/train.py");
-            for(int i = 0 ; i<pythonPM.length;i++) {
-                if (!parStrings.get(pythonPM[i]).equals("\"\"")&&!parStrings.get(pythonPM[i]).equals("")) {
-                        command.add(pythonPM[i]);
-                        command.add(parStrings.get(pythonPM[i]).toString());
+            for (int i = 0; i < pythonPM.length; i++) {
+                if (!parStrings.get(pythonPM[i]).equals("\"\"") && !parStrings.get(pythonPM[i]).equals("")) {
+                    command.add(pythonPM[i]);
+                    command.add(parStrings.get(pythonPM[i]).toString());
                 }
             }
 
-            Thread thread = new Thread(new Runnable()  {
+            Thread thread = new Thread(new Runnable() {
                 @Override
-                public void run()  {
+                public void run() {
                     excPython(command);
-                 }
+                }
             });
             thread.setName(parStrings.get("-q ").toString());
             thread.start();
@@ -61,16 +62,16 @@ public class MlRestApiService {
             threads.add(thread);
             removeThread(threads);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             log.error(e.getMessage());
         }
 
     }
 
-    public void pyStop(String seq){
-        for (int i = 0 ; i<threads.size();i++){
-            if(threads.get(i).getName().equals(seq)){
+    public void pyStop(String seq) {
+        for (int i = 0; i < threads.size(); i++) {
+            if (threads.get(i).getName().equals(seq)) {
                 threads.get(i).interrupt();
 
                 //중단 코드 작성 필요
@@ -85,7 +86,7 @@ public class MlRestApiService {
         try {
             CommandLine commandLine = CommandLine.parse(command.get(0));
             for (int i = 1, n = command.size(); i < n; i++) {
-                commandLine.addArgument(command.get(i),false);
+                commandLine.addArgument(command.get(i), false);
             }
 
             DefaultExecutor executor = new DefaultExecutor();
@@ -93,35 +94,46 @@ public class MlRestApiService {
             int result = executor.execute(commandLine);
             System.out.println("Start result: " + result);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage());
         }
     }
 
-    private void removeThread(ArrayList<Thread> threads){
-        for(int i = 0 ; i <threads.size();i++) {
-            System.out.println(i +"번째 쓰레드:"+threads.get(i).getState().toString()+ " 상태 :"+ threads.get(i).isAlive());
-            if(threads.get(i).isAlive()==false){
+    private void removeThread(ArrayList<Thread> threads) {
+        for (int i = 0; i < threads.size(); i++) {
+            System.out.println(i + "번째 쓰레드:" + threads.get(i).getState().toString() + " 상태 :" + threads.get(i).isAlive());
+            if (threads.get(i).isAlive() == false) {
                 threads.remove(i);
-                i=0;
+                i = 0;
             }
-            System.out.println(i +"번째 쓰레드:"+threads.get(i).getState().toString()+ " 상태 :"+ threads.get(i).isAlive());
+            System.out.println(i + "번째 쓰레드:" + threads.get(i).getState().toString() + " 상태 :" + threads.get(i).isAlive());
         }
     }
 
-    private void stopThread(String model_id){
+    private void stopThread(String model_id) {
         ModelInfoEntity modelInfoEntity = modelInfoRepository.findBySeq(Integer.valueOf(model_id));
         modelInfoEntity.setStatus("중단");
         modelInfoRepository.save(modelInfoEntity);
     }
 
-    public String pythonResponse(String seq) throws Exception{
+    public String pythonResponse(String seq, String data) throws Exception{
         java.io.ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        CommandLine commandline = CommandLine.parse("python3 /app/ad/api/get_model_data.py -m " + Integer.valueOf(seq));
+        String pythonCommand = null;
+        CommandLine commandLine = null;
+        if (data.equals("")) {
+            pythonCommand = "python3 /app/ad/api/get_model_data.py -m " + Integer.valueOf(seq);
+            commandLine = CommandLine.parse(pythonCommand);
+        } else {
+            pythonCommand = "python3 /app/ad/api/TholdChange.py -seq " + Integer.valueOf(seq) + " -data ";
+            commandLine = CommandLine.parse(pythonCommand);
+            commandLine.addArgument(data, false);
+        }
+        System.out.println(commandLine);
         DefaultExecutor exec = new DefaultExecutor();
         PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
         exec.setStreamHandler(streamHandler);
-        exec.execute(commandline);
+
+        exec.execute(commandLine);
         return outputStream.toString();
     }
 }
